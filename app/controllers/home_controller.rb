4e87f48cc89b9c43dc4e5b663a6fba7d824fe352
Cameron Cundiff <ckundo@gmail.com>
2013-08-22 00:06:14 -0400

    Fix URI encoding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 require 'uri'
 class HomeController < ApplicationController
   def show
-    @url = params[:url]
-    if @url && @url.match(URI.regexp)
-      @results ||= AccessLint::Audit.new(@url).run
+    if params[:url]
+      @url = URI::unescape(params[:url])
+      if @url.match(URI.regexp)
+        @results ||= AccessLint::Audit.new(@url).run
+      end
     end
   end
 end
