f9c9785b9623619b93a137818b4616d247075cbe
ckundo <dev@ckundo.com>
2013-09-11 18:31:18 -0400

    Use HEAD of access_lint gem

--- spec/features/audit_spec.rb
+++ spec/features/audit_spec.rb
@@ -1,16 +1,31 @@
 require 'spec_helper'
 
 describe 'Auditing a page' do
-  before do
-    audit = double(:audit, run: 'dummy report')
-    AccessLint::Audit.stub(:new).and_return(audit)
+  context 'stubbing the request' do
+    before do
+      audit = double(:audit, run: 'dummy report')
+      AccessLint::Audit.stub(:new).and_return(audit)
+    end
+
+    it 'returns a report' do
+      visit '/'
+      page.should have_content 'AccessLint'
+      fill_in 'Url', with: 'http://example.com'
+      click_on 'Run'
+      page.should have_content 'dummy report'
+    end
   end
 
-  it 'returns a report' do
-    visit '/'
-    page.should have_content 'AccessLint'
-    fill_in 'URL', with: 'http://example.com'
-    click_on 'Run'
-    page.should have_content 'dummy report'
+  context 'with a full audit' do
+    it 'does not have javascript errors' do
+      visit '/'
+      fill_in 'Url', with: 'http://google.com'
+      click_on 'Run'
+      page.should_not have_content 'Audit Results'
+
+      fill_in 'Url', with: 'http://html5doctor.com'
+      click_on 'Run'
+      page.should_not have_content 'TypeError'
+    end
   end
 end
