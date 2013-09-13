cf431775a25664172c0e2e75527942c62d30e928
Cameron Cundiff <dev@ckundo.com>
2013-09-13 09:22:19 -0400

    HTML Code Sniffer support

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
   def show
     if params[:url]
       @url = URI::unescape(params[:url])
+      @rules = URI::unescape(params[:rules])
+      
       if @url.match(URI.regexp)
-        @results ||= AccessLint::Audit.new(@url).run
+        @results ||= AccessLint::Audit.new(@url).run(@rules)
       end
     end
   end
--- spec/features/audit_spec.rb
+++ spec/features/audit_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe 'Auditing a page' do
   context 'stubbing the request' do
+    let(:report) { 'dummy report' }
     before do
-      audit = double(:audit, run: 'dummy report')
+      audit = double(:audit)
+      audit.stub(:run).with('html_codesniffer').and_return(report)
+      audit.stub(:run).with('google_accessibility_developer_tools').and_return(report)
       AccessLint::Audit.stub(:new).and_return(audit)
     end
 
@@ -11,6 +14,10 @@ describe 'Auditing a page' do
       visit '/'
       page.should have_content 'AccessLint'
       fill_in 'Url', with: 'http://example.com'
+      click_on 'Run'
+      page.should have_content 'dummy report'
+
+      select 'HTML CodeSniffer', from: 'Rules'
       click_on 'Run'
       page.should have_content 'dummy report'
     end
