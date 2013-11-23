0cab9d179f3455664cf22c7b32814a7f7c1e01a0
Cameron Cundiff <dev@ckundo.com>
2013-11-23 12:58:48 -0500

    Revise formatting and guard against timeout failures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,13 @@ class HomeController < ApplicationController
 
       if @url.match(URI.regexp)
         raw_results = AccessLint::Audit.new(@url).run(rules)
-        @results = raw_results.split("\n").select{|message| message.split('|').first == 'ERROR'}
+        raw_results = raw_results.split("\n").select{|message| message.split('|').first == 'ERROR'}
+        @results = raw_results.collect { |result| result.gsub('ERROR|', '').split('|') }
+        flash[:notice] = "Audit complete."
       end
     end
+  rescue Timeout::Error => e
+    flash[:error] = e.message
+    @results = []
   end
 end
