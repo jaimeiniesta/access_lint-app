769f8940e728977d1c612a40865cc2705c7535c2
Cameron Cundiff <dev@ckundo.com>
2013-11-23 17:42:22 -0500

    Use Google ADT rules and display elements that are failing.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,10 @@ class HomeController < ApplicationController
     @results = []
     if params[:url]
       @url = URI::unescape(params[:url])
-      rules = 'html_code_sniffer'
 
       if @url.match(URI.regexp)
-        raw_results = AccessLint::Audit.new(@url).run(rules)
-        raw_results = raw_results.split("\n").select{|message| message.split('|').first == 'ERROR'}
-        @results = raw_results.collect { |result| result.gsub('ERROR|', '').split('|') }
+        raw_results = AccessLint::Audit.new(@url).run
+        @results = raw_results.group_by { |result| result['status'] }
         flash[:notice] = "Audit complete."
       end
     end
