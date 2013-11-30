cb7a2893baded3b51ea204ba717fdbb6e1c2c927
Cameron Cundiff <dev@ckundo.com>
2013-11-29 22:17:28 -0500

    Remove elements from results

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -8,6 +8,7 @@ class Audit
 
   def execute
     raw_results = Rails.cache.read(@url) || AccessLint::Audit.new(@url).run
+    raw_results.map { |result| result.delete('elements') }
     Rails.cache.write(@url, raw_results, expires_in: 1.day)
     @results = raw_results.group_by { |result| result['status'] }
   end
