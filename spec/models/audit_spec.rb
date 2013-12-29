f62c60b4863b0af63f544e6870fcb7bb43c2d6f1
Cameron Cundiff <dev@ckundo.com>
2013-12-29 11:11:35 -0500

    Add test coverage for skipped caching scenario

--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -7,7 +7,19 @@ describe Audit do
       auditor = AccessLint::Audit.new(double)
 
       allow(auditor).to receive(:run).and_raise(JSON::ParserError)
+
       expect{audit.execute}.to raise_error(Audit::ParserError)
+    end
+
+    it 'does not read from the cache if the audit fails' do
+      audit = Audit.new(double)
+      auditor = AccessLint::Audit.new(double)
+
+      allow(auditor).to receive(:run).and_raise(JSON::ParserError)
+      allow(audit).to receive(:execute)
+
+      expect(Rails.cache).not_to receive(:write)
+      audit.execute
     end
   end
 end
