a8ebad38e6528ce7dbf26debdfe3ce8b41817a6e
Cameron Cundiff <dev@ckundo.com>
2013-12-30 20:05:50 -0500

    Update memcachier configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,12 @@
 AccessLintHosted::Application.configure do
-  config.cache_store = :dalli_store,
-                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                    {:username => ENV["MEMCACHIER_USERNAME"],
-                     :password => ENV["MEMCACHIER_PASSWORD"],
-                     :failover => true,
-                     :socket_timeout => 1.5,
-                     :socket_failure_delay => 0.2
-                    }
-
   config.cache_classes = true
+
   config.eager_load = true
 
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  config.cache_store = :dalli_store
 
   client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"],
                            :value_max_bytes => 10485760)
@@ -24,40 +18,25 @@ AccessLintHosted::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
-  config.static_cache_control = "public, max-age=2592000"
+
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+  config.assets.compress = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
-
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
