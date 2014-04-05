0ee0ac37e8d448383ea390f4cff33ae9f1e12073
Cameron Cundiff <dev@ckundo.com>
2014-04-04 21:08:56 -0400

    Revise tests to use new access_lint patterns

--- spec/features/run_audit_spec.rb
+++ spec/features/run_audit_spec.rb
@@ -1,46 +1,6 @@
 require 'spec_helper'
 
 describe 'Auditing a page' do
-  context 'stubbing the request' do
-    let(:report) {
-      [
-        {
-          "element_names"=>[],
-          "severity"=>"Warning",
-          "status"=>"NA",
-          "title"=>"A not applicable condition"
-        },
-        {
-          "element_names"=>[],
-          "severity"=>"Warning",
-          "status"=>"FAIL",
-          "title"=>"A failure condition"
-        },
-        {
-          "element_names"=>[],
-          "severity"=>"Warning",
-          "status"=>"PASS",
-          "title"=>"A passing condition"
-        }
-      ]
-    }
-    before do
-      audit = double(:audit)
-      audit.stub(:run).and_return(report)
-      AccessLint::Audit.stub(:new).and_return(audit)
-    end
-
-    it 'returns a report' do
-      visit '/'
-      page.should have_content 'AccessLint'
-      fill_in 'Url', with: 'http://example.com'
-      click_on 'Run'
-      page.should have_content 'A passing condition'
-      page.should have_content 'A failure condition'
-      page.should have_content 'A not applicable condition'
-    end
-  end
-
   context 'with a full audit' do
     it 'does not have javascript errors' do
       visit '/'
--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -3,11 +3,8 @@ require 'spec_helper'
 describe Audit do
   describe '#execute' do
     it 'rescues JSON::ParseError and yields an Audit exception' do
-      audit = Audit.new(double)
-      auditor = AccessLint::Audit.new(double)
-
-      allow(auditor).to receive(:run).and_raise(JSON::ParserError)
-
+      audit = Audit.new('http://google.com')
+      allow(AccessLint::Audit).to receive(:new).and_raise(JSON::ParserError)
       expect{audit.execute}.to raise_error(Audit::ParserError)
     end
 
