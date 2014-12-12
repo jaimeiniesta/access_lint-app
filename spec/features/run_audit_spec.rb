85b95275d3c275a4dadd2701d94406174b0f8788
Cameron Cundiff <accounts@ckundo.com>
2014-12-12 10:44:00 -0500

    Fix incorrect spec.

--- spec/features/run_audit_spec.rb
+++ spec/features/run_audit_spec.rb
@@ -4,13 +4,9 @@ describe 'Auditing a page' do
   context 'with a full audit' do
     it 'does not have javascript errors' do
       visit '/'
-      fill_in 'Url', with: 'http://google.com'
+      fill_in 'URL to Test', with: 'http://google.com'
       click_on 'Run'
-      page.should_not have_content 'Audit Results'
-
-      fill_in 'Url', with: 'http://html5doctor.com'
-      click_on 'Run'
-      page.should_not have_content 'TypeError'
+      expect(page).to have_content 'Passing'
     end
   end
 end
