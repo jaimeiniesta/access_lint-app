75e318935fa7b58771f7aabf7b65add0d3038c07
Avi Gilligan <avi@thoughtbot.com>
2014-12-18 13:37:55 -0500

    Add clearance for auth
    https://trello.com/c/WDFJQGlS/32-customer-can-create-an-account

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+feature "User signs up" do
+  scenario "successfully" do
+    visit root_path
+    click_link "Sign up for a free trial of AccessLint Pro"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_css "input[value='Sign out']"
+  end
+end
